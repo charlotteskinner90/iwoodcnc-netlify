a3b368aec797333bc2c728c8f266fd7a70064864
Charlotte Skinner <cs.skinner90@hotmail.co.uk>
2020-12-31 17:37:37 +0000

    Switch colour scheme

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram, faFacebookSquare } from "@fortawesome/free-brands-svg-icons"
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faInstagram, faFacebookSquare } from "@fortawesome/free-brands-svg-icons"
+// import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 
 export default function Header() {
   return (
     <header id="home">
-      <nav id="nav-wrap" style={{ display: 'flex', justifyContent: 'space-between', height: '62px' }}>
+      <nav id="nav-wrap" style={{ display: 'flex', justifyContent: 'space-between', height: '62px', backgroundColor: 'white' }}>
         <img className="nav-logo" src="imgs/iwoodbuild.png" alt="iwoodbuild logo" />
         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
@@ -22,17 +22,17 @@ export default function Header() {
       </nav>
       <div className="row banner">
         <div className="banner-text">
-          <img className="logo" src="imgs/iwood_header.png" alt="iwoodbuild logo" />
-          <ul className="social">
+          <img className="logo" src="imgs/iwoodbuild.png" alt="iwoodbuild logo" />
+          {/* <ul className="social">
             <li><a href="https://www.facebook.com/Iwoodcnc-103032651310709"><FontAwesomeIcon icon={faFacebookSquare} /></a></li>
             <li><a href="https://www.instagram.com/iwoodcnc"><FontAwesomeIcon icon={faInstagram} /></a></li>
             <li><a className="smoothscroll" href="#contact"><FontAwesomeIcon icon={faEnvelope} /></a></li>
-          </ul>
+          </ul> */}
         </div>
       </div>
-      <p className="scrolldown">
+      {/* <p className="scrolldown">
         <a className="smoothscroll" href="#about"><i className="icon-down-circle" /></a>
-      </p>
+      </p> */}
     </header>
   );
 }
